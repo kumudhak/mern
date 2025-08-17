a0f4a4245aa65b1b6da8a7a62261f9c0096b6bb5
kumudhak <kumudhak1304@gmail.com>
2025-08-17 19:05:41 +0530

    frontend api cng  commit

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,6 +1,6 @@
 // src/config/api.js
 const API_BASE_URL = process.env.NODE_ENV === 'production' 
-  ? 'https://mern-production-a002.up.railway.app/'  // Replace with your Railway URL
+  ? 'https://mern-production-a002.up.railway.app'  // Replace with your Railway URL
   : 'http://localhost:4000';                   // Local development
 
 export default API_BASE_URL;
